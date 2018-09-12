ff9fdf62677552c6011068ea5613bbfe60a7b9c1
Marcus <marcusmallum@gmail.com>
2018-09-11 22:15:38 -0500

    Comments added

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,10 @@
 /**
- * Created by Marcus on 9/9/2018.
+ * The Node class represents a state in the 8-puzzle problem.
+ * It holds the state, parent node, action, depth, and cost.
+ *
+ * File: Node.java
+ * Author: Marcus Mallum
+ * Date: 9/11/2018
  */
 public class Node {
     String state;
@@ -16,14 +21,22 @@ public class Node {
         setCost(depth);
     }
 
+   /**
+    * Method: setCost
+    * Purpose: Calculate and set the cost of the node
+    * Parameters: int, the depth of the node
+    * Return value: void
+    */
     private void setCost(int depth) {
         cost = 0;
 
+        // Heuristic function
         for(int i = 1; i <= 8; i++) {
             if (state.charAt(i) != 48 + i) {
                 cost++;
             }
         }
+        // Growth function
         cost += depth;
     }
 }
